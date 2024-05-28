0753d83c3b00abd949b1709c9d04e45150f99e17
Chenrui YANG <66559534+ruiiyang@users.noreply.github.com>
2024-05-28 03:19:24 +0200

    testing commit message

--- assignment1.py
+++ assignment1.py
@@ -1,3 +1,4 @@
+
 import pyspark
 from pyspark.sql import SparkSession
 
@@ -6,6 +7,6 @@ spark = SparkSession.builder \
 .master("local") \
 .appName("MySparkApp") \
 .getOrCreate()
-df= spark.read.format("csv").option("header", True).load("lab_dataset.csv")
-
-
+df= spark.read.format("csv").option("header", True).load("./assignment1/lab_dataset.csv")
+df.show()
+spark.stop()
